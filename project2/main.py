e1af724d43699b09fe7d9337e72deccdbce3b278
Aditya Kamble <admin@example.com>
2026-06-23 13:53:32 +0530

    i build my first project

---
+++ project2/main.py
@@ -0,0 +1,10 @@
+print("Namasste , wlecome to Ai chatbot ")
+print("How can I help you ?")
+
+response = {
+    "hello": "Hello! How can I help you ?",
+    "how are you":"i am fine ,thank you for asking ",
+    "who are you ":"i am a ai  smart chatbot",
+}
+
+print(response)
